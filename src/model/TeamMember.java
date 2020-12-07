9ba79cf5fe61bec1da19506b11c97dbef2c66a21
Ion-Caus <causion01@gmail.com>
2020-12-07 21:54:00 +0100

    cleaned code

--- src/model/TeamMember.java
+++ src/model/TeamMember.java
@@ -3,5 +3,17 @@ package model;
 public class TeamMember {
     private String name;
 
+    //TODO divide name to firstName and lastName
+    public TeamMember(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name can not be empty");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 
 }
